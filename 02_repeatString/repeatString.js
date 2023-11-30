5ab55430407738bb67db73121c58190c72acfb82
pedram-pema <studentpedramp@gmail.com>
2023-11-30 11:09:25 +0330

    Complete the exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
-
-};
-
+const repeatString = function(str, num) {
+    let string = str;
+    for(let i = 1; i < num; i++) {
+        string += `${str}`;
+    }
+    if (num == 0) {
+        return '';
+    } else if (num < 0) {
+        return 'ERROR';
+    } else {
+        return string;
+    }
+}
 // Do not edit below this line
 module.exports = repeatString;
