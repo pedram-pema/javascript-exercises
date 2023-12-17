338ff0343dc2c6563b008652aca17fb4ca43f6ba
pedram-pema <studentpedramp@gmail.com>
2023-12-17 12:14:47 +0330

    Complete the exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,12 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let tempInCelsius = (temp - 32) * (5/9);
+  return Math.round(tempInCelsius * 10) / 10;
+  // return tempInCelcius.toFixed(1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let tempInFahrenheit = temp * (9/5) + 32;
+  return Math.round(tempInFahrenheit * 10) / 10;
 };
 
 // Do not edit below this line
